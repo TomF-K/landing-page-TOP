b414d3402f0e66910af436beda127cdff5f78d73
TomF-K <111983192+TomF-K@users.noreply.github.com>
2023-08-10 13:28:08 +0100

    began inputting review content into reviews array

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-
+const reviews [
+    {
+        id: 1,
+        name: "Erik P",
+        location: "MA, USA",
+        text: "Despite having played for 15 years my lessons with Tom provided me with new ways of approaching the instrument."
+    }
+]
