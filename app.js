0baf1af2fe1c6a353b371843679d42748ff70c85
TomF-K <111983192+TomF-K@users.noreply.github.com>
2023-08-14 11:53:37 +0100

    targeted elements using queryselector

--- app.js
+++ app.js
@@ -2,26 +2,33 @@
 const reviews = [
   {
     id: 1,
-    student: "susan smith",
-    location: "web developer",
-    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+    student: "Erik P",
+    location: "MA, USA",
+    text: "Despite having played for 15 years my lessons with Tom provided me with new ways of approaching the instrument.",
   },
   {
     id: 2,
-    student: "anna johnson",
-    location: "web designer",
+    student: "Stew B",
+    location: "Somerset, UK",
     text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
   },
   {
     id: 3,
-    student: "peter jones",
-    location: "intern",
+    student: "Jeff N",
+    location: "MA, USA",
     text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
   },
   {
     id: 4,
-    student: "bill anderson",
-    location: "the boss",
+    student: "Jim D",
+    location: "Worcestshire, UK",
     text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
   },
 ];
+
+// declare variables
+const student = document.querySelector(".author");
+const location = document.querySelector(".location");
+const testimonial = document.querySelector(".testimonial-text");
+
+// add event listeners for button clicks
