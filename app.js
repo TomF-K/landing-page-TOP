7faa69730b1827f383c8b3f1d0947440a0e16046
TomF-K <111983192+TomF-K@users.noreply.github.com>
2023-08-14 12:23:26 +0100

    fixed element targeting and item loading

--- app.js
+++ app.js
@@ -3,32 +3,53 @@ const reviews = [
   {
     id: 1,
     student: "Erik P",
-    location: "MA, USA",
+    hometown: "(MA, USA)",
     text: "Despite having played for 15 years my lessons with Tom provided me with new ways of approaching the instrument.",
   },
   {
     id: 2,
     student: "Stew B",
-    location: "Somerset, UK",
-    text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+    hometown: "(Somerset, UK)",
+    text: "I'm now entering my fifth block of lessons (six lessons per block) and the transformation in my understanding of my instrument, my style of play and my ability to read and write music is little short of miraculous. Tom understands what makes me tick as a person and communicates with such precision, humility and skill that he makes the learning fun and the progression seamless.",
   },
   {
     id: 3,
     student: "Jeff N",
-    location: "MA, USA",
-    text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+    hometown: "(MA, USA)",
+    text: "It's been a while and I've been meaning to email you to let me know how much I appreciate you. So grateful that I found you at a really important time ... my bass guitar formative period. You gave me exactly what was needed! Particularly the left hand fingering thought process. Also in retrospect I even found our dialogue regarding right hand spot on; You were very considerate of my situation and context, and told me how you go about various techniques, while explaining various options.",
   },
   {
     id: 4,
     student: "Jim D",
-    location: "Worcestshire, UK",
-    text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+    hometown: "(Worcestshire, UK)",
+    text: "Tom is a great guy and ultra-professional in his approach to his teaching, has a high quality video-audio setup which he uses to provide lessons, he is very disciplined about documenting our lessons, he always sends me a written plan and objectives following up on lessons and when I send him recordings he always comes to the lesson having listened to them and with comments. My initial reservations about learning over Zoom are gone completely, it works great!",
+  },
+  {
+    id: 5,
+    student: "Alex B",
+    hometown: "(Oxfordshire, UK)",
+    text: "It really is difficult to fault Toms teaching in any way -- he was extremely professional and friendly in our lessons, he had a great audio/video setup, he prepared and shared .pdf files for the lessons with personalised content on whatever i was working on at the time, and also made notes after the lesson so I would know what I should work on in between.",
   },
 ];
 
-// declare variables
+// target elements
 const student = document.querySelector(".author");
-const location = document.querySelector(".location");
+const hometown = document.querySelector(".hometown");
 const testimonial = document.querySelector(".testimonial-text");
+
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+
+// set starting value
+
+let currentItem = 0;
+
+// load starting item
+window.addEventListener("DOMContentLoaded", function () {
+  const item = reviews[currentItem];
+  student.textContent = item.student;
+  hometown.textContent = item.hometown;
+  testimonial.textContent = item.text;
+});
 
 // add event listeners for button clicks
