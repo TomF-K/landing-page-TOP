583b6f30fba2c973c8b9da0907df451e75d88728
TomF-K <111983192+TomF-K@users.noreply.github.com>
2023-08-16 11:52:58 +0100

    unable to position quote markers correctly

--- app.js
+++ app.js
@@ -2,31 +2,31 @@
 const reviews = [
   {
     id: 1,
-    student: "Erik P",
+    student: "Erik P  ",
     hometown: "(MA, USA)",
     text: "Despite having played for 15 years my lessons with Tom provided me with new ways of approaching the instrument.",
   },
   {
     id: 2,
-    student: "Stew B",
+    student: "Stew B  ",
     hometown: "(Somerset, UK)",
     text: "The transformation in my understanding of my instrument, my style of playing and my ability to read and write music is little short of miraculous. Tom understands what makes me tick as a person and communicates with such precision, humility and skill that he makes the learning fun and the progression seamless.",
   },
   {
     id: 3,
-    student: "Jeff N",
+    student: "Jeff N  ",
     hometown: "(MA, USA)",
     text: "It's been a while and I've been meaning to email you to let me know how much I appreciate you... You gave me exactly what was needed! Particularly the left hand fingering thought process. You were very considerate of my situation and context, and told me how you go about various techniques, while explaining various options.",
   },
   {
     id: 4,
-    student: "Jim D",
+    student: "Jim D  ",
     hometown: "(Worcestshire, UK)",
     text: "Tom is a great guy and ultra-professional in his approach to his teaching, has a high quality video-audio setup which he uses to provide lessons, he is very disciplined about documenting our lessons, he always sends me a written plan and objectives following up on lessons and when I send him recordings he always comes to the lesson having listened to them and with comments. My initial reservations about learning over Zoom are gone completely, it works great!",
   },
   {
     id: 5,
-    student: "Alex B",
+    student: "Alex B  ",
     hometown: "(Oxfordshire, UK)",
     text: "It really is difficult to fault Toms teaching in any way -- he was extremely professional and friendly in our lessons, he had a great audio/video setup, he prepared and shared .pdf files for the lessons with personalised content on whatever I was working on at the time, and also made notes after the lesson so I would know what I should work on in between.",
   },
